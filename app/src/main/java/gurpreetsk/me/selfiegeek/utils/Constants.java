2f0bf58fa22faf21826b5e2c92ddbcc4df3bce06
Gurpreet <gurpreetsk@hotmail.com>
2016-11-12 21:59:46 +0530

    feat: add utils

---
+++ app/src/main/java/gurpreetsk/me/selfiegeek/utils/Constants.java
@@ -0,0 +1,17 @@
+package gurpreetsk.me.selfiegeek.utils;
+
+import android.app.Application;
+
+import com.kinvey.android.Client;
+
+/**
+ * Created by Gurpreet on 12/11/16.
+ */
+
+public class Constants {
+
+    public static final int MY_PERMISSIONS_REQUEST_ACCESS_CAMERA = 1;
+    public static final int  MY_PERMISSIONS_REQUEST_RECORD_AUDIO = 2;
+    public static final int CAMERA_RQ = 1001;
+
+}
---
+++ app/src/main/java/gurpreetsk/me/selfiegeek/utils/permissions.java
@@ -0,0 +1,43 @@
+package gurpreetsk.me.selfiegeek.utils;
+
+import android.Manifest;
+import android.app.Activity;
+import android.content.pm.PackageManager;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+
+import static gurpreetsk.me.selfiegeek.utils.Constants.MY_PERMISSIONS_REQUEST_ACCESS_CAMERA;
+import static gurpreetsk.me.selfiegeek.utils.Constants.MY_PERMISSIONS_REQUEST_RECORD_AUDIO;
+
+/**
+ * Created by Gurpreet on 12/11/16.
+ */
+
+public class permissions {
+
+    public static void askCameraPermission(Activity context) {
+        if (ContextCompat.checkSelfPermission(context, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+
+            // Should we show an explanation?
+            if (ActivityCompat.shouldShowRequestPermissionRationale(context, Manifest.permission.CAMERA)) {
+
+            } else {
+                // No explanation needed, we can request the permission.
+                ActivityCompat.requestPermissions(context, new String[]{Manifest.permission.CAMERA}, MY_PERMISSIONS_REQUEST_ACCESS_CAMERA);
+            }
+        }
+    }
+//    public static void askMicrophonePermission(Activity context) {
+//        if (ContextCompat.checkSelfPermission(context, Manifest.permission.RECORD_AUDIO) != PackageManager.PERMISSION_GRANTED) {
+//
+//            // Should we show an explanation?
+//            if (ActivityCompat.shouldShowRequestPermissionRationale(context, Manifest.permission.RECORD_AUDIO)) {
+//
+//            } else {
+//                // No explanation needed, we can request the permission.
+//                ActivityCompat.requestPermissions(context, new String[]{Manifest.permission.RECORD_AUDIO}, MY_PERMISSIONS_REQUEST_RECORD_AUDIO);
+//            }
+//        }
+//    }
+
+}
