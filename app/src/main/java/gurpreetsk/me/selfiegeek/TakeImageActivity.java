cfa0d9adb315c174729c7ddd161a1aacd1829b24
Gurpreet <gurpreetsk@hotmail.com>
2016-11-12 23:20:44 +0530

    feat: added camera code, not showing toasts

--- app/src/main/java/gurpreetsk/me/selfiegeek/TakeImageActivity.java
+++ app/src/main/java/gurpreetsk/me/selfiegeek/TakeImageActivity.java
@@ -1,13 +1,73 @@
 package gurpreetsk.me.selfiegeek;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.support.annotation.NonNull;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.afollestad.materialcamera.MaterialCamera;
+
+import static gurpreetsk.me.selfiegeek.utils.Constants.CAMERA_RQ;
+import static gurpreetsk.me.selfiegeek.utils.Constants.MY_PERMISSIONS_REQUEST_ACCESS_CAMERA;
+import static gurpreetsk.me.selfiegeek.utils.permissions.askCameraPermission;
 
 public class TakeImageActivity extends AppCompatActivity {
+
+    private static final String TAG = "TakeImageActivity";
+    MaterialCamera camera;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_take_image);
+
+        camera = new MaterialCamera(this);
+
+        int permissionCheck = ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA);
+        if (permissionCheck != PackageManager.PERMISSION_GRANTED)
+            askCameraPermission(this);
+        else {
+            camera.stillShot().start(CAMERA_RQ);
+        }
     }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        // Received recording or error from MaterialCamera
+        if (requestCode == CAMERA_RQ) {
+            if (resultCode == RESULT_OK) {
+                Toast.makeText(this, "Saved to: " + data.getDataString(), Toast.LENGTH_LONG).show();
+                Log.i(TAG, "onActivityResult: "+ data.getDataString());
+            } else if (data != null) {
+                Exception e = (Exception) data.getSerializableExtra(MaterialCamera.ERROR_EXTRA);
+                e.printStackTrace();
+                Toast.makeText(this, e.getMessage(), Toast.LENGTH_LONG).show();
+            }
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+
+        switch (requestCode) {
+            case MY_PERMISSIONS_REQUEST_ACCESS_CAMERA: {
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    //permission granted
+                    camera.stillShot().start(CAMERA_RQ);
+                } else {
+                    Toast.makeText(this, "Camera access permission needed to take image", Toast.LENGTH_SHORT).show();
+//                    finish();
+                }
+            }
+        }
+    }
+
 }
